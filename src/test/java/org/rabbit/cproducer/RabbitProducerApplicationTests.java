45305fa68a4f701800b9c511f8fc3bb75068ed50
conroy.white <conroy.white@mastek.com>
2021-04-27 17:11:19 +0100

    initial commit

---
+++ src/test/java/org/rabbit/cproducer/RabbitProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rabbit.cproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
